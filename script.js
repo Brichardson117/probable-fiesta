82b60448b0d9cd288dab31d9cab3bbf54e84b89b
Brittney <brittneyrichardson8@gmail.com>
2022-10-25 17:52:28 -0500

    updated name display

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function displayUser(user) {
  for(let i = 0; i < user.length; i++ ) {
     let primaryDiv = document.createElement('div')
     let userName = document.createElement('h3')
-    userName.textContent = `Name: ${user[i].name}`
+    userName.textContent = `${user[i].name}`
 
     primaryDiv.append(userName);
     document.querySelector("#displayUsers").append(primaryDiv)
