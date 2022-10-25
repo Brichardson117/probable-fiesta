14393ae9a6cff97584fee72758530d43f37e311a
Brittney <brittneyrichardson8@gmail.com>
2022-10-25 17:51:05 -0500

    displayed names successfully

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let users = document.getElementById("user");
 let btn = document.getElementById("btn");
+let postBtn = document.getElementById("postBtn");
 
 function getUsers() {
   let userApi = "https://jsonplaceholder.typicode.com/users";
   fetch(userApi).then(function (response) {
     if (response.ok) {
       response.json().then(function (user) {
-        // displayUser(user);
+        displayUser(user);
         console.log(user);
       });
     } else {
@@ -16,20 +16,29 @@ function getUsers() {
 }
 
 function getPost() {
-    let postApi = "https://jsonplaceholder.typicode.com/posts";
-    fetch(postApi).then(function (response) {
-      if (response.ok) {
-        response.json().then(function (post) {
+  let postApi = "https://jsonplaceholder.typicode.com/posts";
+  fetch(postApi).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (post) {
         //   displayPost(post);
-          console.log(post);
-        });
-      } else {
-        console.log("issue fetching posts at this time");
-      }
-    });
+        console.log(post);
+      });
+    } else {
+      console.log("issue fetching posts at this time");
+    }
+  });
 }
 
+function displayUser(user) {
+ for(let i = 0; i < user.length; i++ ) {
+    let primaryDiv = document.createElement('div')
+    let userName = document.createElement('h3')
+    userName.textContent = `Name: ${user[i].name}`
 
+    primaryDiv.append(userName);
+    document.querySelector("#displayUsers").append(primaryDiv)
+ }
+}
 
-
-btn.addEventListener('click', getPost);
+btn.addEventListener("click", getUsers);
+postBtn.addEventListener("click", getPost);
